a1b56ca534fe7d72830d70c887f081dcd268f8b3
Abhishek Gupta <hi@itsabhishek.me>
2018-11-13 12:21:41 +0530

    initial

---
+++ src/components/BandItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <li @click="toggle">
+    {{band.name}}
+
+    <p v-show="open">
+      {{band.songs | join}}
+      <input type="text" placeholder="Add more songs..." @click.stop @keyup.enter="addSong" v-model="newSong"> 
+    </p>
+
+  </li>
+</template>
+
+
+<script>
+export default {
+  name: 'BandItem',
+  data () {
+    return {
+      newSong: '',
+      open: false
+    }
+  },
+  props: {
+    band: Object
+  },
+  filters: {
+    join (arr) {
+      return arr.join(', ')
+    }
+  },
+  methods: {
+    toggle () {
+      this.open = !this.open
+    },
+    addSong () {
+      this.band.songs.push(this.newSong)
+      this.newSong = ''
+    }
+  }
+}
+</script>
---
+++ src/components/BandsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="text" placeholder="Add Band..">
+    <button> Add </button>
+
+    <ul>
+      <BandItem v-for="band in bands" :key="band.id" :band="band">
+        {{band.name}}
+      </BandItem>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+import BandItem from '@/components/BandItem.vue'
+
+export default {
+  name: 'BandList',
+  components: {
+    BandItem
+  },
+  data () {
+    return {
+      bands: [{
+        id: 1,
+        name: 'Black Sabbath',
+        songs: ['Iron Man', 'War Pigs']
+      }, {
+        id: 2,
+        name: 'ColdPlay',
+        songs: ['Yellow', 'Fix you', 'Mylo xyloto']
+      }]
+    }
+  }
+}
+</script>
